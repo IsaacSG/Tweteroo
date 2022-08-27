3c0d581fc0b9550fa78ede5dd2e22643291e2d4b
Isaac Graciano <isaacgraciano123@gmail.com>
2022-08-26 23:18:36 -0300

    Finished project

--- server.js
+++ server.js
@@ -3,26 +3,38 @@ import cors from "cors";
 
 const server = express();
 
-server.use(cors);
+server.use(cors());
 server.use(express.json());
 
 const User = [];
 const Tweet = [];
+let Cont = 0;
 
 server.post("/sign-up", (req, res) => {
-    User = {username: username, avatar: url};
-    console.log(User);
+    const user = {...req.body};
+    User.push(user);
     res.send("Ok");
 })
 
 server.post("/tweets", (req, res) => {
-    Tweet = {username: username, tweet: tweet};
-    console.log(Tweet);
-    res.send("Ok");
+    let tweet = {...req.body};
+    const findUser = User.filter((user) => user.username === tweet.username);
+    if(findUser != "") {
+        Tweet.push({...req.body, avatar: findUser[0].avatar});
+        Cont ++;
+        res.send("Ok");
+    }
+    else {
+        res.status(401).send("Usuario não existe");
+    }
 })
 
 server.get("/tweets", (req, res) => {
-    res.send(tweets);
+    const Last10 = []
+    for(let i= Cont-10;i < Cont;i++) {
+        Last10.push(Tweet[i]);
+    }
+    res.send(Last10);
 })
 
 server.listen(5000, () => console.log("Listen on 5000"));
